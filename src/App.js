c4a9db2aedc734f5e57019fe9dd63a2ccc05589a
look4regev <look4regev@gmail.com>
2018-07-22 10:06:16 +0300

    [FEAT] Add Google auth lib usage

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import GoogleLogin from 'react-google-login';
+
+const responseGoogle = (response) => {
+  let user_json = JSON.stringify(response.w3);
+  console.log(user_json);
+  localStorage.setItem('user_json', user_json);
+  document.cookie = "user_json=" + user_json;
+}
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React with Google Login</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-intro">
+          <GoogleLogin
+            clientId="COMPLETE YOUR GOOGLE AUTH CLIENT ID FROM GCP"
+            buttonText="Login"
+            onSuccess={responseGoogle}
+            onFailure={responseGoogle}/>
+        </div>
       </div>
     );
   }
